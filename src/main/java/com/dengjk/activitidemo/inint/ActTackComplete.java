1cc20285928fec778d5baf2b0ae0e812514577a2
dengjk <277991520@qq.com>
2019-04-07 23:11:49 +0800

    initi

--- src/main/java/com/dengjk/activitidemo/inint/ActTackComplete.java
+++ src/main/java/com/dengjk/activitidemo/inint/ActTackComplete.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class ActTackComplete {
 
     @Test
-    public void actTaskComplete(){
+    public void actTaskComplete() {
         /**获取默认processEngine对象 1.资源目录下要有activiti.cfg.xml文件 2.bean的id要是processEngineConfiguration*/
         ProcessEngine defaultProcessEngine = ProcessEngines.getDefaultProcessEngine();
         /**获取TaskService对象*/
@@ -31,19 +31,51 @@ public class ActTackComplete {
      * 查询当前人要执行的任务,并且执行完
      */
     @Test
-    public void actTaskCompleteByAssignee(){
+    public void actTaskCompleteByAssignee() {
         /**获取默认processEngine对象 1.资源目录下要有activiti.cfg.xml文件 2.bean的id要是processEngineConfiguration*/
         ProcessEngine defaultProcessEngine = ProcessEngines.getDefaultProcessEngine();
         /**获取TaskService对象*/
         TaskService taskService = defaultProcessEngine.getTaskService();
 
         /**流程实例的的key*/
-        String definitonKey ="holiday";
+        String definitonKey = "holiday";
         List<Task> zhangsanTask = taskService.createTaskQuery().processDefinitionKey(definitonKey).taskAssignee("zhaoliu").list();
         /**获取到某个人的所有任务并且全部执行完*/
         for (Task task : zhangsanTask) {
             taskService.complete(task.getId());
-            System.out.println("任务实例:"+task.getTaskDefinitionKey()+":任务名称"+task.getName()+":执行人"+task.getAssignee());
+            System.out.println("任务实例:" + task.getTaskDefinitionKey() + ":任务名称" + task.getName() + ":执行人" + task.getAssignee());
+        }
+    }
+
+
+    /**
+     * 任务的拾取(就是将多个候选人转换为真正任务的处理人-让任务的assignee有值)
+     * <p>
+     * 当我们在bpmn文件中设置了多个处理人(候选人),我们需要把候选人变成当前处理人
+     */
+    public void actTaskCandidate() {
+        /**首先通过候选人查询当前任务*/
+        /**获取默认processEngine对象 1.资源目录下要有activiti.cfg.xml文件 2.bean的id要是processEngineConfiguration*/
+        ProcessEngine defaultProcessEngine = ProcessEngines.getDefaultProcessEngine();
+        /**获取TaskService对象*/
+        TaskService taskService = defaultProcessEngine.getTaskService();
+        /**流程实例的的key*/
+        String definitonKey = "holiday";
+        String candidate = "zhangsan";
+        List<Task> zhangsanTask = taskService.createTaskQuery().processDefinitionKey(definitonKey).taskCandidateUser(candidate).list();
+        /**获取到某个人的所有任务并且全部执行完*/
+        for (Task task : zhangsanTask) {
+            /**遍历操作把张三的候选人变成处理人  这个时候去查看act_hi_actinst表中的数据就会有当前处理人了*/
+            taskService.claim(task.getId(), candidate);
+            /**再进一步完成当前任务*/
+            taskService.complete(task.getId());
+            System.out.println("任务实例:" + task.getTaskDefinitionKey() + ":任务名称" + task.getName() + ":执行人" + task.getAssignee());
+
+
+            /**===========分割============*/
+            /**任务的交接 ,就是把当前任务的处理人该要处理的人*/
+            taskService.setAssignee(task.getId(), "jiakang");
+
         }
     }
 }
--- src/test/java/com/dengjk/activitidemo/AtivitiTest.java
+++ src/test/java/com/dengjk/activitidemo/AtivitiTest.java
@@ -2,14 +2,34 @@ package com.dengjk.activitidemo;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.RepositoryService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author Dengjk
  * @create 2019-02-21 22:53
- * @desc
+ * @desc 与spring整合测试
  **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/activiti-spring.xml")
 public class AtivitiTest {
 
+
+    @Autowired
+    private RepositoryService repositoryService;
+
+
+    /**
+     *测试注入的对象是否成功
+     */
+    @Test
+    public void test() {
+        System.out.println(repositoryService);
+    }
 
 }
