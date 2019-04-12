7aa22eff901919ac55585d051bab8c41232b0478
dengjk <277991520@qq.com>
2019-04-12 23:22:34 +0800

    initi

--- src/main/java/com/dengjk/activitidemo/service/impl/ActivitiServiceImpl.java
+++ src/main/java/com/dengjk/activitidemo/service/impl/ActivitiServiceImpl.java
@@ -6,9 +6,14 @@ import org.activiti.api.process.model.ProcessDefinition;
 import org.activiti.api.process.runtime.ProcessRuntime;
 import org.activiti.api.runtime.shared.query.Page;
 import org.activiti.api.runtime.shared.query.Pageable;
+import org.activiti.api.task.model.Task;
+import org.activiti.api.task.model.builders.ClaimTaskPayloadBuilder;
+import org.activiti.api.task.model.builders.TaskPayloadBuilder;
 import org.activiti.api.task.runtime.TaskRuntime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Dengjk
@@ -38,7 +43,66 @@ public class ActivitiServiceImpl implements ActivitiService {
      */
     @Override
     public Object findProcessDefinition(Integer pageNo, Integer pageSize) {
+        /**处理登入人*/
+        securityUtil.logInAs("ryandawsonuk");
         Page<ProcessDefinition> processDefinitionPage = processRuntime.processDefinitions(Pageable.of(pageNo, pageSize));
         return processDefinitionPage;
     }
+
+
+    /**
+     * 任务的查看和拾取
+     *
+     * @param name
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
+    public Object findTaskInfo(String name, Integer pageNo, Integer pageSize) {
+        Page<Task> tasks = taskRuntime.tasks(Pageable.of(pageNo, pageSize));
+
+        if (tasks != null && tasks.getTotalItems() > 0) {
+            /**有内容*/
+            List<Task> content = tasks.getContent();
+            for (Task task : content) {
+                /**先任务拾取*/
+                taskRuntime.claim(TaskPayloadBuilder.claim().withTaskId(task.getId()).build());
+
+                /**任务的完成*/
+                taskRuntime.complete(TaskPayloadBuilder.complete().withTaskId(task.getId()).build());
+
+            }
+        }
+        return null;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
